47f122cf208a6592c845935fce6b1795696f74ee
Ldaz54 <ldaz@mail.austral.edu.ar>
2025-04-14 11:46:04 -0300

    Update line.py

--- line.py
+++ line.py
@@ -7,8 +7,8 @@ def line():
     print('El coeficiente B de su ecuación de la recta es: ',float(B))
     print('El coeficiente X1 de su ecuación de la recta es: ',float(X1))
     print('El coeficiente X2 de su ecuación de la recta es: ',float(X2))
-    print(
-        f'''Para la siguiente ecuación
+    print('')
+    print( f'''Para la siguiente ecuación
        Y = {A}x+{float(B)}
      ''')
     print(f'''Dados los siguientes puntos:
@@ -18,4 +18,4 @@ def line():
     
     p = [float(X1),float(A*X1+B)]
     q = [float(X2),float(A*X2+B)]
-    print('La distancia entre ellos es: '(math.dist(p, q)))
+    print(f'La distancia entre ellos es: {math.dist(p, q)}')
