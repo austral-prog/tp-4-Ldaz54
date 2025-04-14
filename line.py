66911f113854223c815f65b673bb894b900dc653
Ldaz54 <ldaz@mail.austral.edu.ar>
2025-04-14 11:55:10 -0300

    Update line.py

--- line.py
+++ line.py
@@ -1,21 +1,17 @@
+import math
 def line():
     A = float(input('Ingrese el coeficiente A: '))
     B = float(input('Ingrese el coeficiente B: '))
     X1 = float(input('Ingrese el coeficiente X1:'))
     X2 = float(input('Ingrese el coeficiente X2: '))
-    print('El coeficiente A de su ecuación de la recta es: ',float(A))
-    print('El coeficiente B de su ecuación de la recta es: ',float(B))
-    print('El coeficiente X1 de su ecuación de la recta es: ',float(X1))
-    print('El coeficiente X2 de su ecuación de la recta es: ',float(X2))
+    print('El coeficiente A de su ecuación de la recta es:',float(A))
+    print('El coeficiente B de su ecuación de la recta es:',float(B))
+    print('El coeficiente X1 de su ecuación de la recta es:',float(X1))
+    print('El coeficiente X2 de su ecuación de la recta es:',float(X2))
     print('')
-    print( f'''Para la siguiente ecuación
-       Y = {A}x+{float(B)}
-     ''')
-    print(f'''Dados los siguientes puntos:
-        P1 ({float(X1)},{float(A*X1+B)}) 
-        P2 ({float(X2)},{float(A*X2+B)})
-        ''')
+    print( f'''Para la siguiente ecuación:\n\tY = {A}X + {float(B)}''')
+    print(f'''\nDados los siguientes puntos:\n\tP1 ({float(X1)}, {float(A*X1+B)})\n\tP2 ({float(X2)}, {float(A*X2+B)})''')
     
     p = [float(X1),float(A*X1+B)]
     q = [float(X2),float(A*X2+B)]
-    print(f'La distancia entre ellos es: {math.dist(p, q)}')
+    print(f'\nLa distancia entre ellos es: {math.dist(p,q)}')
